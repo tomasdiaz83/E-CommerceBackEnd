ec1e04ae361ed63eb410bcdac8bd0936a879c310
TDiaz83 <tomas.enrique.diaz@gmail.com>
2022-10-31 17:12:16 -0500

    category delete operation working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -65,8 +65,22 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy(
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+    res.status(200).json(categoryData);
+  }
+  catch (err) {
+    res.status(500).json(err)
+    console.log(err)
+  }
 });
 
 module.exports = router;
